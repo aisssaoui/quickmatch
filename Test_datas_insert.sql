6988065dbd188fbc43199f561ae9cce40ed365ab
JohanChataigne <57454071+JohanChataigne@users.noreply.github.com>
2019-11-14 15:51:30 +0100

    Test datas file
    SQL script inserting test datas in database
    Only inserting in "Player" for now

---
+++ Test_datas_insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO Player (surname, first_name, scored_goals, conceded_goals, matches_played, victories)
+	VALUES ('Chataigner', 'Johan', 1, 0, 2, 1),
+			('Genty', 'Laurent', 2, 1, 2, 0),
+            ('Issaoui', 'Ali', 2, 2, 2, 1),
+            ('Abderrahmane', 'Faiz', 0, 0, 2, 1),
+            ('Rosay', 'Maxime', 2, 2, 2, 2),
+            ('Hala', 'Mehdi', 2, 1, 2, 1),
+            ('Faverge', 'Mathieu', 100, 0, 1, 0),
+            ('Delarue', 'Tony', 101, 0, 1, 1),
+            ('Meline', 'Raphael', 1, 2, 2, 0),
+            ('Danquigny', 'Antoine', 2, 2, 1, 1);
+            
