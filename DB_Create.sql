b22f19fe3180d1d3c50571a50f1f5e85979a8763
Mehdi HALA <mhala@enseirb-matmeca.fr>
2019-11-20 01:23:52 +0100

    Changing syntax to pgsql

--- DB_Create.sql
+++ DB_Create.sql
@@ -1,44 +1,45 @@
 DROP DATABASE IF EXISTS Quick_Match;
-CREATE DATABASE Quick_Match CHARACTER SET 'utf8';
-USE Quick_Match;
+CREATE DATABASE Quick_Match ENCODING = 'UTF8';
+
+SET client_encoding = 'UTF8';
 
 CREATE TABLE IF NOT EXISTS Player (
-id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+id SERIAL PRIMARY KEY,
 surname VARCHAR(40) NOT NULL,
 first_name VARCHAR(40) NOT NULL,
 mail_address VARCHAR(40),
 phone_number VARCHAR(20),						/*A voir pour le type selon utilisation*/
 address TEXT,
-scored_goals SMALLINT UNSIGNED DEFAULT 0 NOT NULL,
-conceded_goals SMALLINT UNSIGNED DEFAULT 0 NOT NULL,
-matches_played SMALLINT UNSIGNED DEFAULT 0 NOT NULL,
-victories SMALLINT UNSIGNED DEFAULT 0 NOT NULL
+scored_goals SMALLINT DEFAULT 0 NOT NULL,
+conceded_goals SMALLINT DEFAULT 0 NOT NULL,
+matches_played SMALLINT DEFAULT 0 NOT NULL,
+victories SMALLINT DEFAULT 0 NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Club (				/* Table groupe*/
-id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+id SERIAL PRIMARY KEY,
 title VARCHAR(40) NOT NULL,						/*Nom du groupe*/
-creation_date DATETIME NOT NULL
+creation_date DATE NOT NULL
 );
 
 /*Pas de table feuille stat car (1,1)-(1,1)*/
 
 CREATE TABLE IF NOT EXISTS Slot (
-id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+id SERIAL PRIMARY KEY,
 start_hour TIME NOT NULL,
 end_hour TIME NOT NULL,
 repeat_day VARCHAR(9)									/*Champ a preciser*/
 );
 
 CREATE TABLE IF NOT EXISTS Invitation (
-id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+id SERIAL PRIMARY KEY,
 event_type VARCHAR(20)  						/*type discutable si liste finie d'event*/
 );
 
 CREATE TABLE IF NOT EXISTS Meet (				/*Nom a revoir peut etre*/
-id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-slot SMALLINT UNSIGNED,
-invitation SMALLINT UNSIGNED,
+id SERIAL PRIMARY KEY,
+slot SMALLINT,
+invitation SMALLINT,
 FOREIGN KEY (slot) REFERENCES Slot(id),
 FOREIGN KEY (invitation) REFERENCES Invitation(id),
 location TEXT, 									/*A voir pour le type*/
@@ -46,35 +47,35 @@ minimal_team_size SMALLINT
 );
 
 CREATE TABLE IF NOT EXISTS Player_Belong_Club (
-player SMALLINT UNSIGNED,
-club SMALLINT UNSIGNED,
-is_admin TINYINT UNSIGNED,						/*Trouver un meilleur equivalent aux booléens maybe*/
+player SMALLINT,
+club SMALLINT,
+is_admin SMALLINT,						/*Trouver un meilleur equivalent aux booléens maybe*/
 FOREIGN KEY (player) REFERENCES Player(id),
 FOREIGN KEY (club)   REFERENCES Club(id),
 PRIMARY KEY (player, club)
 );
 
 CREATE TABLE IF NOT EXISTS Player_Received_Invitation (
-player SMALLINT UNSIGNED,
-invitation SMALLINT UNSIGNED,
-accepted TINYINT UNSIGNED, 						/*Same problem boolean*/
+player SMALLINT,
+invitation SMALLINT,
+accepted SMALLINT, 						/*Same problem boolean*/
 FOREIGN KEY (player) REFERENCES Player(id),
 FOREIGN KEY (invitation) REFERENCES Invitation(id),
 PRIMARY KEY (player, invitation)
 );
 
 CREATE TABLE IF NOT EXISTS Player_Played_Meet (
-player SMALLINT UNSIGNED,
-meet SMALLINT UNSIGNED,
-won TINYINT UNSIGNED, 							/*Same problem boolean*/
+player SMALLINT,
+meet SMALLINT,
+won SMALLINT, 							/*Same problem boolean*/
 FOREIGN KEY (player) REFERENCES Player(id),
 FOREIGN KEY (meet) REFERENCES Meet(id),
 PRIMARY KEY (player, meet)
 );
 
 CREATE TABLE IF NOT EXISTS Player_Available_Slot ( /*Ajout d'un boolean ?*/
-player SMALLINT UNSIGNED,
-slot SMALLINT UNSIGNED,
+player SMALLINT,
+slot SMALLINT,
 FOREIGN KEY (player) REFERENCES Player(id),
 FOREIGN KEY (slot) REFERENCES Slot(id),
 PRIMARY KEY (player, slot)
