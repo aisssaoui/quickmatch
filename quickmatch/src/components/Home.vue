664f42d18653fb2de6bd7e5d532ef13bdaeca002
Ali Issaoui <aissaoui@enseirb-matmeca.fr>
2019-11-30 14:13:38 +0100

    improvement

--- quickmatch/src/components/Home.vue
+++ quickmatch/src/components/Home.vue
@@ -28,15 +28,52 @@
 
     <!-- Jumbotron -->
     <div class="container-fluid">
-      <div class="row jumbotron">
+      <div id="jumb" class="row jumbotron">
         <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-10">
           <p class="lead">Avec Quick Match, une meilleure organisation de vos matchs.</p>
         </div>
-          <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-2">
-            <a href="#"><button type="button" class="btn btn-outline-secondary btn-lg">Regoinez nous</button></a>
-          </div>
+        <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-2">
+          <a href="#">
+            <button id="reg" type="button" class="btn btn-outline-secondary btn-lg">rejoignez nous</button>
+          </a>
         </div>
       </div>
+    </div>
+
+    <!-- Welcome Section -->
+    <div class="container-fluid padding">
+      <div class="row welcome text-center">
+        <div class="col-12">
+          <h1 class="display-4">Une meilleure expérience</h1>
+        </div>
+        <hr />
+        <div class="col-12">
+          <p class="lead">Organisez, Jouez et Mémorisez vos meilleurs moments avec Quick Match!</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Three Column Section -->
+    <div class="container-fluid padding">
+    <div class="row text-center padding">
+        <div class="col-sm-6 col-md-4">
+            <img src="../assets/pro.png" style="width:80px;height:70px;">
+            <h3 id="foot">PRO FOOT</h3>
+            <p>Match de football professionel.</p>
+        </div>
+        <div class="col-sm-6 col-md-4">
+            <img src="../assets/street.png" style="width:70px;height:70px;">
+            <h3 id="foot">STREET FOOT</h3>
+            <p>Match de professionel de salle.</p>
+        </div>
+        <div class="col-sm-6 col-md-4">
+            <img src="../assets/pro.png" style="width:80px;height:70px;">
+            <h3 id="foot">PRO FOOT</h3>
+            <p>Match de football professionel.</p>
+        </div>
+    </div>
+    <hr class="my-4">
+    </div>
   </div>
 </template>
 
@@ -74,5 +111,17 @@ a {
 }
 .display-2 {
   margin-bottom: 0px;
+}
+.lead {
+  padding: 12px 0px 0px 0px;
+}
+#reg {
+  margin-right: 10px;
+}
+#foot{
+  margin-top: 15px
+}
+#jumb {
+  padding: 20px 10px 20px 10px
 }
 </style>
