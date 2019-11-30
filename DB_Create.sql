b9aac37114d13fc46370b0ab40b0089dadd880c5
antoine_danquigny <danquigny16@gmail.com>
2019-11-30 17:05:45 +0100

    maj mysql->postgresql creation bdd

--- DB_Create.sql
+++ DB_Create.sql
@@ -1,87 +1,83 @@
 DROP DATABASE IF EXISTS Quick_Match;
-CREATE DATABASE Quick_Match CHARACTER SET 'utf8';
-USE Quick_Match;
+CREATE DATABASE Quick_Match WITH ENCODING 'UTF8';
+\c quick_match;
+
 
 CREATE TABLE IF NOT EXISTS Player (
-id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+id SERIAL PRIMARY KEY CHECK (id >= 0),
 surname VARCHAR(40) NOT NULL,
 first_name VARCHAR(40) NOT NULL,
 mail_address VARCHAR(40),
 phone_number VARCHAR(20),						/*A voir pour le type selon utilisation*/
 address TEXT,
-scored_goals SMALLINT UNSIGNED DEFAULT 0 NOT NULL,
-conceded_goals SMALLINT UNSIGNED DEFAULT 0 NOT NULL,
-matches_played SMALLINT UNSIGNED DEFAULT 0 NOT NULL,
-victories SMALLINT UNSIGNED DEFAULT 0 NOT NULL
+scored_goals SMALLINT DEFAULT 0 NOT NULL CHECK (scored_goals >= 0),
+conceded_goals SMALLINT DEFAULT 0 NOT NULL CHECK (conceded_goals >= 0),
+matches_played SMALLINT DEFAULT 0 NOT NULL CHECK (matches_played >= 0),
+victories SMALLINT DEFAULT 0 NOT NULL CHECK (victories >= 0)
 );
 
 CREATE TABLE IF NOT EXISTS Club (				/* Table groupe*/
-id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+id SERIAL PRIMARY KEY CHECK (id >= 0),
 title VARCHAR(40) NOT NULL,						/*Nom du groupe*/
-creation_date DATETIME NOT NULL,
-private TINYINT UNSIGNED
+creation_date TIMESTAMP NOT NULL,
+private BOOLEAN
 );
 
 /*Pas de table feuille stat car (1,1)-(1,1)*/
 
 CREATE TABLE IF NOT EXISTS Slot (
-id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+id SERIAL PRIMARY KEY CHECK (id >= 0),
 start_hour TIME NOT NULL,
 end_hour TIME NOT NULL,
 repeat_day VARCHAR(9)									/*Champ a preciser*/
 );
 
 CREATE TABLE IF NOT EXISTS Invitation (
-id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-slot SMALLINT UNSIGNED,
-player SMALLINT UNSIGNED,
+id SERIAL PRIMARY KEY CHECK (id >= 0),
+slot SMALLINT CHECK (slot >= 0),
+player SMALLINT CHECK (player >= 0),
 FOREIGN KEY (slot) REFERENCES Slot(id),
 FOREIGN KEY (player) REFERENCES Player(id),
 event_type VARCHAR(20)  						/*type discutable si liste finie d'event*/
 );
 
 CREATE TABLE IF NOT EXISTS Meet (				/*Nom a revoir peut etre*/
-id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-slot SMALLINT UNSIGNED,
-invitation SMALLINT UNSIGNED,
+id SERIAL PRIMARY KEY CHECK (id >= 0),
+slot SMALLINT CHECK (slot >= 0),
+invitation SMALLINT CHECK (invitation >= 0),
 FOREIGN KEY (slot) REFERENCES Slot(id),
 FOREIGN KEY (invitation) REFERENCES Invitation(id),
 location TEXT,
-precise_date DATE,								
+precise_date TIMESTAMP,
 minimal_team_size SMALLINT,
 maximal_team_size SMALLINT,
-deletion_date DATE
+deletion_date TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS Player_Belong_Club (
-player SMALLINT UNSIGNED,
-club SMALLINT UNSIGNED,
-is_admin TINYINT UNSIGNED,						/*Trouver un meilleur equivalent aux booléens maybe*/
+player SMALLINT CHECK (player >= 0),
+club SMALLINT CHECK (club >= 0),
+is_admin BOOLEAN,						/*Trouver un meilleur equivalent aux booléens maybe*/
 FOREIGN KEY (player) REFERENCES Player(id),
 FOREIGN KEY (club)   REFERENCES Club(id),
 PRIMARY KEY (player, club)
 );
 
 CREATE TABLE IF NOT EXISTS Meet_Sheet (
-player SMALLINT UNSIGNED,
-meet SMALLINT UNSIGNED,
-scored_goals SMALLINT UNSIGNED DEFAULT 0 NOT NULL,
-conceded_goals SMALLINT UNSIGNED DEFAULT 0 NOT NULL,
-won TINYINT UNSIGNED, 							/*Same problem boolean*/
+player SMALLINT CHECK (player >= 0),
+meet SMALLINT CHECK (meet >= 0),
+scored_goals SMALLINT CHECK (scored_goals >= 0) DEFAULT 0 NOT NULL,
+conceded_goals SMALLINT CHECK (conceded_goals >= 0) DEFAULT 0 NOT NULL,
+won BOOLEAN, 							/*Same problem boolean*/
 FOREIGN KEY (player) REFERENCES Player(id),
 FOREIGN KEY (meet) REFERENCES Meet(id),
 PRIMARY KEY (player, meet)
 );
 
 CREATE TABLE IF NOT EXISTS Invitation_For_Meet (
-invitation SMALLINT UNSIGNED,
-meet SMALLINT UNSIGNED,
+invitation SMALLINT CHECK (invitation >= 0),
+meet SMALLINT CHECK (meet >= 0),
 FOREIGN KEY (invitation) REFERENCES Invitation(id),
 FOREIGN KEY (meet) REFERENCES Meet(id),
 PRIMARY KEY (invitation, meet)
 );
-
-/*ENGINE = INNODB; Pour chaque table si on utilise mySQL*/
-
-
-	
