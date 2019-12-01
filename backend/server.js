c8034644516b20c06d650f3050f3b0b8420a49d8
Abderrahmane Faiz <abderrahmane.faiz.50@gmail.com>
2019-12-01 23:03:31 +0100

    avancement côté backend

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import 'babel-polyfill';
+import Player from './src/controllers/player';
+
+
+dotenv.config();
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  return res.status(200).send({'message': 'YAY! Congratulations! Your first endpoint is working'});
+});
+
+app.post('/api/v1/Players', Player.create);
+app.get('/api/v1/Players', Player.getAll);
+app.get('/api/v1/Players/:id', Player.getOne);
+app.put('/api/v1/Players/:id', Player.update);
+app.delete('/api/v1/Players/:id', Player.delete);
+
+app.listen(3000)
+console.log('app running on port ', 3000);
