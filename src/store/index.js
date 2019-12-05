f19254bd6fcc0fe68ead72b7a54ba7406a428778
JohanChataigne <jchataigner@enseirb-matmeca.fr>
2019-12-05 16:14:55 +0100

    start adding google button

--- src/store/index.js
+++ src/store/index.js
@@ -5,25 +5,80 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        isLoggedIn: false,
+        givenName: "",
+        name: "", // nom complet
+        familyName: "",
+        email: "",
+        isSignedIn: false,
+        connectionDate: Object,
+        expirationDate: Object,
+        expirationTimer: 0,
+        googleUser: Object,
+        profile: Object,
+        flag: 0 // to detect if googleUser is filled or not
+
     },
     mutations: {
-        login (state) {
-            state.isLoggedIn = true
+        setGoogleUser(state, googleUser) {
+            state.flag = 1;
+            state.googleUser = googleUser;
+            state.profile = googleUser.getBasicProfile();
+            state.givenName = state.profile.getGivenName();
+            state.name = state.profile.getName();
+            state.familyName = state.profile.getFamilyName();
+            state.email = state.profile.getEmail();
+            state.isSignedIn = state.googleUser.isSignedIn();
+            state.expirationTimer = state.googleUser.getAuthResponse().expires_in;
+            /*** Setting expiration timer ***/
+            state.connectionDate = new Date();
+            state.expirationDate = new Date();
+            state.expirationDate.setSeconds(state.connectionDate.getSeconds() + state.expirationTimer);
         },
-        logout (state) {
-            state.isLoggedIn = false
+        isSignedIn(state) {
+            if (state.flag) {
+                var now = new Date();
+                state.isSignedIn = ( now < state.expirationDate);
+            }
+        },
+        logout(state) {
+            state.flag = 0;
+            state.isSignedIn = false;
+            state.googleUser.disconnect();
         }
     },
     actions: {
-        login ({commit}) {
-            commit('login')
+        setGoogleUser(commit, googleUser) {
+            this.commit("setGoogleUser", googleUser);
         },
-        logout ({commit}) {
-            commit('logout')
+        isSignedIn(commit) {
+            this.commit("isSignedIn");
+        },
+        logout(commit) {
+            this.commit("logout");
         }
     },
     getters: {
-        isLoggedIn: state => { return state.isLoggedIn }
+        givenName: function(state) {
+            return state.givenName;
+        },
+        name: function(state) {
+            return state.name;
+        },
+        familyName: function(state) {
+            return state.familyName;
+        },
+        email: function(state) {
+            return state.email;
+        },
+        isSignedIn: function(state) {
+            return state.isSignedIn;
+        },
+        connectionDate: function(state) {
+            return state.connectionDate;
+        },
+        expirationDate: function(state) {
+            return state.expirationDate;
+        }
     }
 });
+
