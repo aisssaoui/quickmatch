630a246c7674fb6c3447e2f62f69329fd802167b
LaurentGENTY <laurentgenty1@gmail.com>
2020-02-09 18:47:55 +0100

    indenting

--- App_Mobile/app/src/main/java/com/example/quickmatch/BaseFragment.kt
+++ App_Mobile/app/src/main/java/com/example/quickmatch/BaseFragment.kt
@@ -6,5 +6,7 @@ import androidx.fragment.app.Fragment
 
 /* Class to allow changing title of a fragment in the app bar */
 abstract class BaseFragment : Fragment() {
-    fun getActionBar() : androidx.appcompat.app.ActionBar? { return (activity as AppCompatActivity).supportActionBar }
+    fun getActionBar() : androidx.appcompat.app.ActionBar? {
+        return (activity as AppCompatActivity).supportActionBar
+    }
 }
